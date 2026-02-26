fad71947ce0351156174a512a5ff5e016ede56eb
kaif <kaif.vittal313@gmail.com>
2026-02-26 16:16:11 +0530

    setup custom logging

--- rag_agent.py
+++ rag_agent.py
@@ -13,15 +13,17 @@ from dotenv import load_dotenv
 import os
 
 
-# DEBUG
 import logging
 LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', 'INFO')
-logging.basicConfig(filename="rag_agent_logs.log",
-                    filemode="a",
-                    format="[{levelname}] {asctime} :({name}): {message}",
-                    level=LOGGING_LEVEL,
-                    datefmt="%Y-%m-%d %H:%M:%s", style="{")
-# DEBUG END
+
+formatter = logging.Formatter(fmt="[{levelname}] {asctime} :({name}): {message}",
+                              datefmt="%Y-%m-%d %H:%M:%s", style="{")
+file_handler = logging.FileHandler(filename="rag_agent_logs.log", mode="a", encoding='utf-8')
+file_handler.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
+logger.addHandler(file_handler)
+logger.setLevel(LOGGING_LEVEL)
 
 
 load_dotenv()
@@ -35,38 +37,38 @@ class RAGAgent:
         self.tools = {tool.name: tool for tool in tools}
         self.llm = ChatOllama(model=MODEL).bind_tools(tools)
         self.history: list[BaseMessage] = [SystemMessage(SYSTEM_PROMPT)]
-        logging.info("Agent set up successfully")
+        logger.info("Agent set up successfully")
 
     def _run_tool(self, tool_calls) -> list[ToolMessage]:
         tool_results = []
         for tool in tool_calls:
             tool_name, tool_args = tool['name'], tool['args']
-            logging.info(f"Calling tool {tool_name}({(tool_args)})")
+            logger.info(f"Calling tool {tool_name}({(tool_args)})")
             try:
                 tool_result = self.tools[tool_name].func(**tool_args)
             except Exception as e:
-                logging.error(e)
+                logger.error(e, exc_info=True)
             tool_results.append(ToolMessage(content=tool_result, tool_call_id=tool['id']))
         return tool_results
 
     def ask(self, user_message: str):
-        logging.info("Control over to AI.")
+        logger.info("Control over to AI.")
         self.history.append(HumanMessage(content=user_message))
         messages = self.history.copy()
         new_messages_index = len(messages)
         for i in range(MAX_ITERATIONS):
-            logging.debug(f"Iteration {i}. {len(messages)=}, {new_messages_index=}")
+            logger.debug(f"Iteration {i}. {len(messages)=}, {new_messages_index=}")
             ai_message = AIMessage("")
 
             tools_to_use = []
             for ai_msg_chunk in self.llm.stream(messages):
                 if ai_msg_chunk.tool_calls:
-                    logging.info(f"Tool detected: {ai_msg_chunk.tool_calls}.")
+                    logger.info(f"Tool detected: {ai_msg_chunk.tool_calls}.")
                     tools_to_use.extend(ai_msg_chunk.tool_calls)
                 ai_message.content += str(ai_msg_chunk.content)
                 yield ai_msg_chunk.content
 
-            logging.debug(f"AI message is {ai_message.content}. Tools to use: {[tool['name'] for tool in tools_to_use]}")
+            logger.debug(f"AI message is {ai_message.content}. Tools to use: {[tool['name'] for tool in tools_to_use]}")
             if ai_message.content:
                 messages.append(ai_message)
                 break
